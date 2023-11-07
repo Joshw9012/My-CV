dfa9bcb5830892807f32e7c6b78c1516f0c5d596
Josh <105602925+Joshw9012@users.noreply.github.com>
2023-11-07 15:34:20 +1100

    Update About.js
    fix alt='''

--- src/Components/About.js
+++ src/Components/About.js
@@ -20,7 +20,7 @@ class About extends Component {
               <img
                 className="profile-pic"
                 src={profilepic}
-                alt="David Profile"
+                alt="JW Profile"
               />
             </div>
             <div className="nine columns main-col">
